c2c5ecab5e2527205874d47a4979f8c9ca9dc80b
SeanSyue <32350820+SeanSyue@users.noreply.github.com>
2025-01-26 23:27:59 +0100

    Add result table parser

--- src/main/java/org/tensorflow/model/examples/App.java
+++ src/main/java/org/tensorflow/model/examples/App.java
@@ -1,6 +1,8 @@
 package org.tensorflow.model.examples;
 
-import com.google.common.collect.Table;
+import java.io.IOException;
+
+import org.tensorflow.model.examples.cnn.fastrcnn.DetectionResultParser;
 import org.tensorflow.model.examples.cnn.fastrcnn.FasterRcnnInception;
 import org.tensorflow.model.examples.cnn.lenet.CnnMnist;
 import org.tensorflow.model.examples.cnn.vgg.VGG11OnFashionMnist;
@@ -8,41 +10,26 @@ import org.tensorflow.model.examples.dense.SimpleMnist;
 import org.tensorflow.model.examples.regression.linear.LinearRegressionExample;
 import org.tensorflow.model.examples.tensors.TensorCreation;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+import com.google.common.collect.Table;
 
 public class App {
 
-    // private static final String COCO_LABELS_FILE = "coco-labels-2017.txt";
-
-    private static String[] loadExternalLabels(String labelFileName) throws IOException {
-    /**
-     * Load COCO dataset labels from an extertal text file.
-     */
-        String strFilePath = ClassLoader.getSystemResource(labelFileName).getPath();
-        Path filePath = Paths.get(strFilePath);
-        List<String> lines = Files.lines(filePath).collect(Collectors.toList());
-        String[] labelsArray = lines.toArray(new String[0]);
-        return labelsArray;
-    }
-
     private static void printTable(Table<Integer, String, Float> table) throws IOException {
-        // Create a mapping of each row in the table
-        Map<Integer, Map<String, Float>> map = table.rowMap();
+
+        // Create a parser for parsing the detection results
+        DetectionResultParser parser = new DetectionResultParser();
+        
+        // Initialize the parser with the table
+        parser.load(table);
 
         // Iterate the row mapping print the data of each row
-        for (Integer row : map.keySet()) {
-            System.out.println("Detection class: " + loadExternalLabels("coco-labels-2017.txt")[Math.round(map.get(row).get("detection_class")) - 1]);
-            System.out.println("Detection score: " + map.get(row).get("detection_score"));
-            System.out.println("ymin: " + map.get(row).get("ymin"));
-            System.out.println("xmin: " + map.get(row).get("xmin"));
-            System.out.println("ymax: " + map.get(row).get("ymax"));
-            System.out.println("xmax: " + map.get(row).get("xmax"));
+        for (Integer row : parser.getKeySetPerRow()) {
+            System.out.println("Detection class: " + parser.getLabelByRow(row));
+            System.out.println("Detection score: " + parser.getScoreByRow(row));
+            System.out.println("ymin: " + parser.getYminByRow(row));
+            System.out.println("xmin: " + parser.getXminByRow(row));
+            System.out.println("ymax: " + parser.getYmaxByRow(row));
+            System.out.println("xmax: " + parser.getXmaxByRow(row));
             System.out.println("------------------");
         }
     }
